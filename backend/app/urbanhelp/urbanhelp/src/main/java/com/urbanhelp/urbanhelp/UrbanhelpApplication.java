19aa72163945cfe9cd69d1b9bdc7b90a4698604d
Blancaaa7 <blancaperezc77@gmail.com>
2026-04-05 11:02:04 +0200

    creado el inicio de backend

---
+++ backend/app/urbanhelp/urbanhelp/src/main/java/com/urbanhelp/urbanhelp/UrbanhelpApplication.java
@@ -0,0 +1,13 @@
+package com.urbanhelp.urbanhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanhelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanhelpApplication.class, args);
+	}
+
+}
